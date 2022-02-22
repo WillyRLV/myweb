2f42b81a2fa9a218d9ddbb6171e6168212423361
WillyRLV <williamlv1998@gmail.com>
2022-02-22 11:02:43 -0500

    Mejorando pagina web

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-/*NAVARRRRRR SCRIPT MOBILE*/
-const list = document.querySelectorAll('.list');
-  function activeLink(){
-    list.forEach(
-      (item) =>
-    item.classList.remove('active2')
-    );
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
-   this.classList.add('active2');
-  }
-  list.forEach((item) => 
-  item.addEventListener('click',activeLink));
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+
+
+
+  /** FADE PAGE*/
+
+  const sec1Bg = document.getElementById('home')
+  window.addEventListener('scroll', function(){
+    sec1Bg.style.opacity = 1 - +window.pageYOffset/550+ '';
+  });
+  
